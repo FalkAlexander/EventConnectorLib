5f6f445adf80c80fe64b183fb2fbc8db586c6a4f
Falk Seidl <falk.seidl@iosb-ast.fraunhofer.de>
2024-05-29 14:54:37 +0000

    docs(config): Update Sphinx documentation configuration
    This commit updates the Sphinx documentation configuration in the config.py file and adds some necessary files and code lines.
    Changes include:
    - The importation of os and sys modules at the top of the config.py file.
    - The addition of "sphinx.ext.autodoc" and "sphinx.ext.napoleon" to the list of extensions that help in generating documentation from docstrings written in Google or NumPy style.
    - The insertion of the project's source directory into the system path to allow Sphinx to locate the modules.
    - Copyright and author information have been updated.
    - The creation of a new file, modules.rst, that holds the documentation for the various modules in the project.
    - Update in index.rst file to include the automodule directive that automatically generates documentation from the docstrings in the specified module..

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,23 +9,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'EventConnectorLib'
-copyright = '2024, Falk Seidl'
-author = 'Falk Seidl'
-release = '0.1'
+project = "EventConnectorLib"
+copyright = "2024, Fraunhofer IOSB-AST"
+author = "Falk Seidl"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # If you're using Google or NumPy style docstrings
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+sys.path.insert(0, os.path.abspath("../src"))
---
+++ docs/modules.rst
@@ -0,0 +1,23 @@
+event_connector_lib
+===================
+
+.. automodule:: event_connector_lib
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+client
+======
+
+.. automodule:: event_connector_lib.client
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+utils
+=====
+
+.. automodule:: event_connector_lib.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
